5beb7bd636bd2141ab8d8e10bd4861200616ec5a
Elivelton Pereira <elivelton.pereira@academico.ifpb.edu.br>
2023-06-09 11:27:37 -0300

    Criando formulário no front e ajustes de interface

--- src/indextests.js
+++ src/indextests.js
@@ -2,7 +2,9 @@
 let buttonShowList = document.querySelector('#menuBarButton')
 let buttonExitList = document.querySelector('#exitListButton')
 let listEvents = document.querySelector('.event-list')
+let form = document.querySelector('.form-register')
 let menuBar = document.querySelector('.menu-bar')
+let buttonForm = document.querySelector('#exitForm')
 
 let clickOne = 0
 
@@ -14,5 +16,9 @@ buttonShowList.addEventListener('click', () => {
 buttonExitList.addEventListener('click', () => {
     listEvents.classList.add('hide')
     buttonShowList.classList.remove('hide')
+})
+
+buttonForm.addEventListener('click', () => {
+    form.classList.add('hide')
 })
 
